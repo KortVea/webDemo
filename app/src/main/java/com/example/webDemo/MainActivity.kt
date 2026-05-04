3ef7fd0b2f6cbdf86f500c9095ac87df9c89da95
isaah <sirrvea@gmail.com>
2026-05-04 22:59:28 +1000

    accompanist.webview

--- app/src/main/java/com/example/webDemo/MainActivity.kt
+++ app/src/main/java/com/example/webDemo/MainActivity.kt
@@ -7,11 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.webDemo.ui.theme.WebDemoTheme
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +19,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             WebDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    val state = rememberWebViewState("https://www.google.com")
+                    WebView(
+                        state = state,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WebDemoTheme {
-        Greeting("Android")
     }
 }
